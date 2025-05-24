13e99c815b35e2b4fa392774fb199b74bd5de295
Your Name <your.email@example.com>
2025-05-25 03:04:28 +0530

    arrays commit

---
+++ basics/array.js
@@ -0,0 +1,22 @@
+const myArr=[0,1,2,3,4 ]
+const bp=['lisa','jennie']
+const bp2= new Array(1,2,3,4)
+console.log(bp)
+
+//array methods
+
+bp.push('rosie')  //pop also
+
+bp.unshift('jisoo') //shift also 
+console.log(bp)
+
+console.log(bp.indexOf('lisa'))
+//.join()  binds the array and converts to sring 
+
+//slice 
+console.log(bp.slice(1,2))
+
+// splice 
+console.log(bp.splice(1,2))
+
+console.log(`original array ${bp}`)
